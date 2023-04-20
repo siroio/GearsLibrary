8630daf1294452179c39de38ad5e0fe474d844c5
RukaHoriuchi <g021g1011@g.neec.ac.jp>
2023-04-20 12:51:43 +0900

    add: Build Batch FIle

---
+++ Build.bat
@@ -0,0 +1,31 @@
+@echo off
+@chcp 65001 > nul
+
+set BUILD_DIR=Build
+set IS_PAUSE=Flase
+
+if not exist %BUILD_DIR%\nul (
+  echo %BUILD_DIR%フォルダが存在しないため、作成します。
+  mkdir %BUILD_DIR%
+)
+
+echo CMakeのビルドを開始します...
+cmake -S . -B %BUILD_DIR%
+if errorlevel 1 (
+  echo CMakeのビルドに失敗しました。
+  pause
+  exit /b 1
+)
+
+echo makeを開始します...
+cmake --build %BUILD_DIR%
+if errorlevel 1 (
+  echo ビルドに失敗しました。
+  pause
+  exit /b 1
+)
+
+echo ビルドが正常に終了しました。
+if %IS_PAUSE% equ True (
+  pause
+)
