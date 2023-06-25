54959cfa9861eeb158707316ef330976b77f6b60
RukaHoriuchi <g021g1011@g.neec.ac.jp>
2023-06-25 15:15:34 +0900

    add: json library from to utility

---
+++ GearsUtility/include/Json.h
@@ -0,0 +1,6 @@
+#ifndef GEARS_JSON_H
+#define GEARS_JSON_H
+
+#include <nlohmann/json.hpp>
+
+#endif // !GEARS_JSON_H
---
+++ GearsUtility/include/Jsonfwd.h
@@ -0,0 +1,6 @@
+#ifndef GEARS_JSON_FWD_H
+#define GEARS_JSON_FWD_H
+
+#include <nlohmann/json_fwd.hpp>
+
+#endif // !GEARS_JSON_FWD_H
