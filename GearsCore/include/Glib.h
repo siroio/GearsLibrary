d9b2dbfea7c45e6d911da168cd1788e894be9d46
RukaHoriuchi <g021g1011@g.neec.ac.jp>
2023-06-25 15:13:29 +0900

    update: add directxtexd.lib directxtex.lib

--- GearsCore/include/Glib.h
+++ GearsCore/include/Glib.h
@@ -12,8 +12,24 @@
 
 #ifdef _DEBUG
 /* DEBUG BUILD LIB */
+#pragma comment(lib, "GearsCored.lib")
+#pragma comment(lib, "GearsComponentd.lib")
+#pragma comment(lib, "GearsGraphicsd.lib")
+#pragma comment(lib, "GearsMathd.lib")
+#pragma comment(lib, "GearsObjectd.lib")
+#pragma comment(lib, "GearsScened.lib")
+#pragma comment(lib, "GearsUtilityd.lib")
+#pragma comment(lib, "DirectXTexd.lib")
 #else
 /* RELEASE BUILD LIB */
+#pragma comment(lib, "GearsCore.lib")
+#pragma comment(lib, "GearsComponent.lib")
+#pragma comment(lib, "GearsGraphics.lib")
+#pragma comment(lib, "GearsMath.lib")
+#pragma comment(lib, "GearsObject.lib")
+#pragma comment(lib, "GearsScene.lib")
+#pragma comment(lib, "GearsUtility.lib")
+#pragma comment(lib, "DirectXTex.lib")
 #endif // _DEBUG
 
 #endif // !GEARS_LIBRARY_H
