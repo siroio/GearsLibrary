db7d5b9b9be0d34ffd30a10f9ffd51e018c1f387
RukaHoriuchi <g021g1011@g.neec.ac.jp>
2023-09-11 06:51:42 +0900

    add: resource id

--- GearsGraphics/include/Internal/DX12/GraphicsResourceID.h
+++ GearsGraphics/include/Internal/DX12/GraphicsResourceID.h
@@ -1,9 +1,9 @@
 #pragma once
+#define UCHAR unsigned char
+#define UINT unsigned int
 
 namespace Glib::Internal::Graphics::ID
 {
-    using UCHAR = unsigned char;
-
     /* 頂点リソースID */
 
     constexpr UCHAR CAMERA_VERTEX = 0;
@@ -27,9 +27,32 @@ namespace Glib::Internal::Graphics::ID
     constexpr UCHAR SPRITE_SHADER = 0;
     constexpr UCHAR CAMERA_SHADER = SPRITE_SHADER + 1;
     constexpr UCHAR IMAGE_SHADER = CAMERA_SHADER + 1;
+    constexpr UCHAR LINE_SHADER = IMAGE_SHADER + 1;
+    constexpr UCHAR MESH_SHADER = LINE_SHADER + 1;
+    constexpr UCHAR MESH_SHADOW_SHADER = MESH_SHADER + 1;
+
+    /*  シェーダーパラメーターID  */
+
+    constexpr UINT MESH_ALBEDO = 0;
+    constexpr UINT MESH_NORMAL_MAP = 1;
+    constexpr UINT MESH_MATERIAL_BUFFER = 4;
+
+    constexpr UINT MESH_CAMERA_CONSTANT = 2;
+    constexpr UINT MESH_WORLD_MATRIX = 3;
+    constexpr UINT MESH_DIRECTIONAL_LIGHT = 5;
+    constexpr UINT MESH_SHADOW_MAP = 6;
+
+    constexpr UINT SKINNED_MESH_CAMERA_CONSTANT = 2;
+    constexpr UINT SKINNED_MESH_WORLD_MATRIX = 3;
+    constexpr UINT SKINNED_MESH_SKINNED_MATRIX = 5;
+    constexpr UINT SKINNED_MESH_DIRECTIONAL_LIGHT = 6;
+    constexpr UINT SKINNED_MESH_SHADOW_MAP = 7;
 
     /* テクスチャID */
 
     constexpr UCHAR WHITE_TEXTURE = 0;
     constexpr UCHAR NORMAL_MAP_TEXTURE = WHITE_TEXTURE + 1;
 }
+
+#undef UCHAR
+#undef UINT
