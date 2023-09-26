571c334c441675205d206a2f08a7e10950122c6a
RukaHoriuchi <g021g1011@g.neec.ac.jp>
2023-09-26 10:54:27 +0900

    update: camera shader code

---
+++ GearsGraphics/include/Internal/DX12/ShaderCode/CameraShader.h
@@ -0,0 +1,41 @@
+#pragma once
+
+namespace Glib::Internal::Graphics::ShaderCode
+{
+    /**
+     * @brief カメラ用シェーダー
+     */
+    constexpr char CAMERA_SHADER[] =
+    {
+        "Texture2D<float4> tex : register(t0);          \
+        SamplerState smp : register(s0);                \
+                                                        \
+        struct VSInput                                  \
+        {                                               \
+            float4 position : POSITION;                 \
+        };                                              \
+                                                        \
+        struct VSOutput                                 \
+        {                                               \
+            float4 position : SV_POSITION;              \
+            float2 uv : TEXCOORD;                       \
+        }                                               \
+                                                        \
+        typedef VSOutput PSInput;                       \
+                                                        \
+        VSOutput VSMain(VSInput input)                  \
+        {                                               \
+            VSOutput o;                                 \
+            o.position = input.position;                \
+            o.position.xy = o.position.xy * 2.0f - 1.0f;\
+            o.position.y *= -1;                         \
+            o.uv = input.position.xy;                   \
+        }                                               \
+                                                        \
+        float4 PSMain(PSInput input)                    \
+        {                                               \
+            return tex.Sample(smp, input.uv)            \
+        }                                               \
+        "
+    };
+}
