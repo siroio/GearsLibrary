4917d9039def03585a5b358474509de3f3bf23ac
RukaHoriuchi <g021g1011@g.neec.ac.jp>
2023-09-29 16:39:38 +0900

    update: shader header

--- GearsGraphics/include/Internal/DX12/ShaderCode/MeshShader.h
+++ GearsGraphics/include/Internal/DX12/ShaderCode/MeshShader.h
@@ -2,5 +2,8 @@
 
 namespace Glib::Internal::Graphics::ShaderCode
 {
+    //TODO: フォンシェーディング実装
+    constexpr char MESH_SHADER[]{ "" };
 
+    constexpr char MESH_SHADOW_SHADER[]{ "" };
 }
--- GearsGraphics/include/Internal/DX12/ShaderCode/SkinnedMeshShader.h
+++ GearsGraphics/include/Internal/DX12/ShaderCode/SkinnedMeshShader.h
@@ -2,6 +2,8 @@
 
 namespace Glib::Internal::Graphics::ShaderCode
 {
+    //TODO: フォンシェーディング実装
+    constexpr char SKINNED_MESH_SHADER[]{ "" };
 
-
+    constexpr char SKINNED_MESH_SHADOW_SHADER[]{ "" };
 }
