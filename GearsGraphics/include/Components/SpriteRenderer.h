6f778d7905404cb37bcaa3501cc04b5df816682e
RukaHoriuchi <g021g1011@g.neec.ac.jp>
2023-10-06 16:41:28 +0900

    add: sprite renderer

---
+++ GearsGraphics/include/Components/SpriteRenderer.h
@@ -0,0 +1,118 @@
+#pragma once
+#include <Internal/Renderer.h>
+#include <Internal/DX12/ConstantBuffer.h>
+#include <Internal/DX12/VertexBuffer.h>
+#include <Vector2.h>
+#include <Color.h>
+
+namespace Glib::Internal
+{
+    class CameraBase;
+}
+
+namespace Glib
+{
+    class Transform;
+}
+
+namespace Glib
+{
+    class SpriteRenderer : public Internal::Renderer
+    {
+    public:
+        SpriteRenderer();
+
+        void Start();
+        void LateUpdate();
+        void Draw(const WeakPtr<Internal::CameraBase>& camera);
+
+
+        /**
+         * @brief 中心位置を取得
+         */
+        const Vector2& Center() const;
+
+        /**
+         * @brief 中心位置を設定
+         */
+        void Center(const Vector2& center);
+
+        /**
+         * @brief 色を取得 
+         */
+        const Color& Color() const;
+
+        /**
+         * @brief 色を設定
+         */
+        void Color(const ::Color& color);
+
+        /**
+         * @brief 左右反転しているか 
+         */
+        bool FlipX() const;
+
+        /**
+         * @brief 左右反転を設定
+         */
+        void FlipX(bool flip);
+
+        /**
+         * @brief 上下反転しているか
+         */
+        bool FlipY() const;
+
+        /**
+         * @brief 上下反転を設定
+         */
+        void FlipY(bool flip);
+
+        /**
+         * @brief クリッピング位置を取得 
+         * @brief (ピクセル単位)
+         */
+        const Vector2& ClippingPosition() const;
+
+        /**
+         * @brief クリッピング位置を設定
+         * @brief (ピクセル単位)
+         */
+        void ClippingPosition(const Vector2& position);
+
+        /**
+         * @brief クリッピングサイズを取得
+         * @brief (ピクセル単位)
+         */
+        const Vector2& ClippingSize() const;
+
+        /**
+         * @brief クリッピングサイズを設定
+         * @brief (ピクセル単位)
+         */
+        void ClippingSize(const Vector2& size);
+
+        /**
+         * @brief テクスチャIDを取得
+         */
+        unsigned int TextureID() const;
+
+        /**
+         * @brief テクスチャIDを設定
+         */
+        void TextureID(unsigned int id, bool isResetSize = true);
+
+    private:
+        bool enabled_{ false };
+        Internal::Graphics::ConstantBuffer constantBuffer_{};
+        Internal::Graphics::VertexBuffer vertexBuffer_{};
+        bool flipX_{ false };
+        bool flipY_{ false };
+        unsigned int textureID_{ UINT_MAX };
+        Vector2 center_{ 0.5f, 0.5f };
+        Vector2 textureSize_{ 0.0f, 0.0f };
+        Vector2 clippingPosition_{ 0.0f, 0.0f };
+        Vector2 clippingSize_{ 0.0f, 0.0f };
+        ::Color color_{ Color::White() };
+        WeakPtr<Transform> transform_;
+    };
+}
---
+++ GearsGraphics/src/Components/SpriteRenderer.cpp
@@ -0,0 +1,185 @@
+#include <Components/SpriteRenderer.h>
+#include <Internal/DX12/DirectX12.h>
+#include <Internal/DX12/GraphicsResource.h>
+#include <Internal/DX12/GraphicsResourceID.h>
+#include <Internal/RenderingManager.h>
+#include <Internal/CameraBase.h>
+#include <TextureManager.h>
+#include <GameObject.h>
+#include <Mathf.h>
+#include <Vector3.h>
+
+using namespace  Glib::Internal::Graphics;
+
+namespace
+{
+    auto s_dx12 = DirectX12::Instance();
+    auto s_resource = GraphicsResource::Instance();
+    auto s_renderingManager = RenderingManager::Instance();
+    auto& s_textureManager = Glib::TextureManager::Instance();
+
+    struct SpriteConstant
+    {
+        Vector3 position{ 0.0f, 0.0f, 0.0f };
+        float angle{ 0.0f };
+        Vector2 scale{ 1.0f, 1.0f };
+        Vector2 center{ 0.5f, 0.5f };
+        Vector2 spriteSize{ 0.0f, 0.0f };
+        float padding[2]{ 0.0f, 0.0f };
+        Color color{ 1.0f, 1.0f, 1.0f, 1.0f };
+    };
+
+    struct SpriteVertex
+    {
+        Vector2 position{ 0.0f, 0.0f };
+        Vector2 uv{ 0.0f, 0.0f };
+    };
+}
+
+Glib::SpriteRenderer::SpriteRenderer()
+{
+    constantBuffer_.Create(sizeof(SpriteConstant));
+    vertexBuffer_.Create(sizeof(SpriteVertex), 4);
+}
+
+void Glib::SpriteRenderer::Start()
+{
+    transform_ = GameObject()->Transform();
+    s_renderingManager->AddRenderer(WeakPtr<SpriteRenderer>{ shared_from_this() }.get());
+}
+
+void Glib::SpriteRenderer::LateUpdate()
+{
+    if (!enabled_) return;
+
+    // 定数バッファ
+    SpriteConstant buffer{};
+    buffer.position = transform_->Position();
+    buffer.angle = transform_->EulerAngles().z * Mathf::DEG2RAD;
+    buffer.scale = transform_->Scale();
+    buffer.center = center_;
+    buffer.spriteSize = clippingSize_ / 100.0f;
+    buffer.color = color_;
+    constantBuffer_.Update(sizeof(buffer), &buffer);
+
+    //切り取りしたUV 座標計算
+    // 左上
+    Vector2 leftTopUV = clippingPosition_;
+    leftTopUV.x /= textureSize_.x;
+    leftTopUV.y /= textureSize_.y;
+
+    // 右下
+    Vector2 rightBottomUV;
+    rightBottomUV.x = leftTopUV.x + (clippingSize_.x / textureSize_.x);
+    rightBottomUV.y = leftTopUV.y + (clippingSize_.y / textureSize_.y);
+
+    // UV反転
+    if (flipX_) std::swap(leftTopUV.x, rightBottomUV.x);
+    if (flipY_) std::swap(leftTopUV.y, rightBottomUV.y);
+
+    // 頂点バッファ
+    SpriteVertex vertices[]
+    {
+        // position, uv
+        { Vector2{ 0.0f, 0.0f }, Vector2{ leftTopUV.x,          leftTopUV.y } },
+        { Vector2{ 0.0f,-1.0f }, Vector2{ leftTopUV.x,      rightBottomUV.y } },
+        { Vector2{ 1.0f, 0.0f }, Vector2{ rightBottomUV.x,      leftTopUV.y } },
+        { Vector2{ 1.0f,-1.0f }, Vector2{ rightBottomUV.x,  rightBottomUV.y } }
+    };
+
+    // 頂点バッファ更新
+    vertexBuffer_.Update(vertices);
+}
+
+void Glib::SpriteRenderer::Draw(const WeakPtr<Internal::CameraBase>& camera)
+{
+    if (!enabled_) return;
+
+    vertexBuffer_.SetBuffer();
+
+    camera->SetConstantBuffer(1);
+
+    s_resource->SetPipelineState(ID::SPRITE_PIPELINESTATE);
+
+    constantBuffer_.BindPipeline(2);
+
+    s_textureManager.SetTexture(textureID_, 0);
+
+    s_dx12->CommandList()->IASetPrimitiveTopology(D3D10_PRIMITIVE_TOPOLOGY_TRIANGLESTRIP);
+    s_dx12->CommandList()->DrawInstanced(4, 1, 0, 0);
+}
+
+const Vector2& Glib::SpriteRenderer::Center() const
+{
+    return center_;
+}
+
+void Glib::SpriteRenderer::Center(const Vector2& center)
+{
+    center_ = center;
+}
+
+const Color& Glib::SpriteRenderer::Color() const
+{
+    return color_;
+}
+
+void Glib::SpriteRenderer::Color(const::Color& color)
+{
+    color_ = color;
+}
+
+bool Glib::SpriteRenderer::FlipX() const
+{
+    return flipX_;
+}
+
+void Glib::SpriteRenderer::FlipX(bool flip)
+{
+    flipX_ = flip;
+}
+
+bool Glib::SpriteRenderer::FlipY() const
+{
+    return flipY_;
+}
+
+void Glib::SpriteRenderer::FlipY(bool flip)
+{
+    flipY_ = flip;
+}
+
+const Vector2& Glib::SpriteRenderer::ClippingPosition() const
+{
+    return clippingPosition_;
+}
+
+void Glib::SpriteRenderer::ClippingPosition(const Vector2& position)
+{
+    clippingPosition_ = position;
+}
+
+const Vector2& Glib::SpriteRenderer::ClippingSize() const
+{
+    return clippingSize_;
+}
+
+void Glib::SpriteRenderer::ClippingSize(const Vector2& size)
+{
+    clippingSize_ = size;
+}
+
+unsigned int Glib::SpriteRenderer::TextureID() const
+{
+    return textureID_;
+}
+
+void Glib::SpriteRenderer::TextureID(unsigned int id, bool isResetSize)
+{
+    if (!s_textureManager.Contains(id)) return;
+    enabled_ = true;
+    textureSize_ = s_textureManager.TextureSize(id);
+    if (isResetSize) clippingSize_ = textureSize_;
+}
+
+
