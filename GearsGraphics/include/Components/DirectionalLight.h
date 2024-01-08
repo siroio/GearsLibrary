5479edc08af3d439c16515f8d19c12d09126a3da
RukaHoriuchi <g021g1011@g.neec.ac.jp>
2024-01-08 13:54:19 +0900

    add: directional light

---
+++ GearsGraphics/include/Components/DirectionalLight.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+#include <Component.h>
+#include <Components/Transform.h>
+#include <WeakPtr.h>
+
+struct Color;
+
+namespace Glib
+{
+    class DirectionalLight : public Component
+    {
+    public:
+        void Start();
+        void LateUpdate();
+        void Ambient(const Color& ambient);
+        void Diffuse(const Color& diffuse);
+        void Specular(const Color& specular);
+        void OnGUI();
+    private:
+        WeakPtr<Transform> transform_;
+    };
+}
---
+++ GearsGraphics/src/Components/DirectionalLight.cpp
@@ -0,0 +1,61 @@
+﻿#include <Components/DirectionalLight.h>
+#include <Internal/RenderingManager.h>
+#include <GameObject.h>
+#include <GLGUI.h>
+#include <Color.h>
+#include <imgui.h>
+
+using namespace Glib::Internal::Graphics;
+
+void Glib::DirectionalLight::Start()
+{
+    transform_ = GameObject()->Transform();
+}
+
+void Glib::DirectionalLight::LateUpdate()
+{
+    RenderingManager::LightDirection(transform_->Forward());
+}
+
+void Glib::DirectionalLight::Ambient(const Color& ambient)
+{
+    RenderingManager::LightAmbient(ambient);
+}
+
+void Glib::DirectionalLight::Diffuse(const Color& diffuse)
+{
+    RenderingManager::LightDiffuse(diffuse);
+}
+
+void Glib::DirectionalLight::Specular(const Color& specular)
+{
+    RenderingManager::LightSpecular(specular);
+}
+
+void Glib::DirectionalLight::OnGUI()
+{
+    Component::OnGUI();
+    if (GLGUI::TreeNode("Ambient"))
+    {
+        Color ambient = RenderingManager::LightAmbient();
+        if (GLGUI::ColorInput3("##Ambient", &ambient))
+            Ambient(ambient);
+        GLGUI::TreePop();
+    }
+
+    if (GLGUI::TreeNode("Diffuse"))
+    {
+        Color diffuse = RenderingManager::LightDiffuse();
+        if (GLGUI::ColorInput3("##Diffuse", &diffuse))
+            Diffuse(diffuse);
+        GLGUI::TreePop();
+    }
+
+    if (GLGUI::TreeNode("Specular"))
+    {
+        Color specular = RenderingManager::LightSpecular();
+        if (GLGUI::ColorInput3("##Specular", &specular))
+            Specular(specular);
+        GLGUI::TreePop();
+    }
+}
