751761b153c9106f986025a2f0984888990d2153
RukaHoriuchi <g021g1011@g.neec.ac.jp>
2024-06-24 10:21:23 +0900

    fix: consistency in object destruction

--- GearsScene/src/SceneManager.cpp
+++ GearsScene/src/SceneManager.cpp
@@ -1,17 +1,20 @@
 ﻿#include <SceneManager.h>
 #include <Scene.h>
 #include <GameObjectManager.h>
+#include <Internal/DX12/DirectX12.h>
 #include <Debugger.h>
 
 namespace
 {
     auto s_gameObjectManager = Glib::GameObjectManager::Instance();
+    auto s_dx12 = Glib::Internal::Graphics::DirectX12::Instance();
 }
 
 void Glib::SceneManager::Update()
 {
     if (next_.expired()) return;
     if (!current_.expired()) current_->End();
+    s_dx12->WaitGPUPrevFrame();
     s_gameObjectManager->ResetGameObjects();
     s_gameObjectManager->RemoveDestroyableObjects();
     current_ = next_;
